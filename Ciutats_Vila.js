fea2dc10eb09f442866ebf9e049802fe2cbefec4
aaron <aaronamiell@gmail.com>
2020-10-23 15:41:10 +0200

    arreglo "" bcn

--- Ciutats_Vila.js
+++ Ciutats_Vila.js
@@ -1,8 +1,8 @@
-function PrintCities() {
-    var a = ["Lleida", Barcelona", "Paris"];
-    a.forEach(function(entry) {
-        console.log(entry);
-    });
+function  PrintCities()  {    
+    var  a  =   ["Lleida", "Barcelona",  "Paris"];    
+    a.forEach(function(entry)  {        
+        console.log(entry);    
+    });
 }
 
 PrintCities();
